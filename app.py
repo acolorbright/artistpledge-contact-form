246e2e0bd81d33ab084354f630462ff36e1428fc
Alice Rose <alice@acolorbright.com>
2017-03-09 16:15:35 +0100

    Begin converting app.py to use Mailgun instead of Mandrill.

--- app.py
+++ app.py
@@ -1,32 +1,35 @@
 import os
 
-from mandrill import Mandrill
 from flask import Flask, request, redirect, abort
 
 app = Flask(__name__)
-mandrill_client = Mandrill(os.environ['MANDRILL_API_KEY'])
+
+MAILGUN_API_KEY = os.environ['MAILGUN_API_KEY']
+SANDBOX_DOMAIN = os.environ['SANDBOX_DOMAIN']
+SITE_ADDRESS = os.environ['SITE_ADDRESS']
+TO_NAME = os.environ['USER_NAME']
+TO_EMAIL = os.environ['USER_EMAIL']
 
 
 @app.route('/')
 def index():
-    return redirect(os.environ['USER_SITE'])
+    return redirect(SITE_ADDRESS)
 
 
 @app.route('/send', methods=['POST'])
-def forward():
-    message = {
-               'to': [{'email': os.environ['USER_EMAIL']}],
-               'from_email': request.form['email'],
-               'subject': 'Message from {}, Subject:{}'.format(request.form['name'], request.form['subject']),
-               'text': request.form['message'],
-              }
-    result = mandrill_client.messages.send(message=message)
-    if result[0]['status'] != 'sent':
-        abort(500)
-    return redirect(os.environ['SUCCESS_PAGE'])
+def send_simple_message():
+    return requests.post(
+        'https://api.mailgun.net/v3/{}.mailgun.org/messages'.format(SANDBOX_DOMAIN),
+        auth=('api', MAILGUN_API_KEY),
+        data={
+            'from': '{0} <{1}>'.format(request.form['name'], request.form['email']),
+            'to': '{0} <{1}>'.format(TO_NAME, TO_EMAIL)
+            'subject': 'Message from {}'.format(request.form['name']),
+            'text': request.form['mesage'],
+        }
+)
 
 
 @app.errorhandler(500)
 def error(e):
     return ('Sorry, something went wrong! %s - %s' % (e.__class__, e), 500)
-
